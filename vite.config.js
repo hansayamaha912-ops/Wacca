7b97b6185c9b3be45d0149e04d4e7e881e14951a
hospital-scraper <hansayamaha912@gmail.com>
2026-05-30 23:43:05 +0900

    fix: modify vercelPreset import syntax for commonjs compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import {defineConfig} from 'vite';
 import {hydrogen} from '@shopify/hydrogen/vite';
 import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
-import {vercelPreset} from '@vercel/remix';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+// 直接 named import せず、デフォルトインポートしてから展開する
+import vercelRemixPkg from '@vercel/remix';
+const { vercelPreset } = vercelRemixPkg;
 
 export default defineConfig({
   plugins: [
